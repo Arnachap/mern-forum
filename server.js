9529ac7f29c86e152e9afddcb05f445362b5a957
arnachap <arnaud.chapeleur@gmail.com>
2019-10-09 10:19:21 +0200

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Define routes
 app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
